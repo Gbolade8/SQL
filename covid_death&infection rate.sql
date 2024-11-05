1313fc557f5b5d708b7fe4d572e4edc75c8ad041
Gbolade <149517881+Gbolade8@users.noreply.github.com>
2024-11-05 20:02:46 +0000

    Add files via upload

---
+++ covid_death&infection rate.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE 'covid_19' ('country' TEXT,'continent' TEXT,'population' REAL,'day' TEXT,'time' TEXT,'Cases' INTEGER,'Recovered' REAL,'Deaths' REAL,'Tests' REAL);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
